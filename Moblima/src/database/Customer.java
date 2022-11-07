f00efaeb2edf59a427a8a6184e0028562e7b942a
Sherylleong <sherylleongkitye@gmail.com>
2022-11-07 13:35:52 +0800

    Update Customer.java

--- Moblima/src/database/Customer.java
+++ Moblima/src/database/Customer.java
@@ -4,19 +4,25 @@ import enums.AgeGroup;
 import enums.UserType;
 
 public class Customer extends User{
+    /*
+     * Creates a Customer with a given name and username, age, mobile number, email address and history of movies watched
+     * @param age Age of this customer
+     * @param ageGroup Age group of this customer
+     * @param mobileNumber Mobile number of this customer
+     * @param emailAddress Email address of this customer
+     * @param movieHistory List of movies previously watched by this customer
+     */
+    
     private int age;
     private AgeGroup ageGroup;
     private int mobileNumber;
     private String emailAddress;
     ArrayList<Movie> movieHistory;
-    private int cardHolder;
-    
 
-   
-
-    public Customer(String firstName, String lastName, int age, int mobileNumber, String emailAddress, Movie[] movieHistory, int cardHolder){
-        super(firstName, lastName); super.setUserType(UserType.CUSTOMER); setAge(age);; this.mobileNumber = mobileNumber; this.emailAddress = emailAddress; this.cardHolder = cardHolder;
+    public Customer(String firstName, String lastName, int age, int mobileNumber, String emailAddress){
+        super(firstName, lastName); super.setUserType(UserType.CUSTOMER); setAge(age);; this.mobileNumber = mobileNumber; this.emailAddress = emailAddress;
     }
+
     public int getAge(){
         return this.age;
     }
@@ -56,14 +62,6 @@ public class Customer extends User{
 
     public void setEmailAdress(String emailAdress){
         this.emailAddress = emailAdress;
-    }
-
-    public int getCardHolder(){
-        return this.cardHolder;
-    } 
-
-    public void setCardHolder(int cardHolder){
-        this.cardHolder = cardHolder;
     }
 
     // implement movie history if possible
