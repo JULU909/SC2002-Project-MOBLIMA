d9a3c6c77468f106ff4a9d5b69217c8205da806e
tham holdon <HTHAM001@e.ntu.edu.sg>
2022-11-08 23:22:04 +0800

    Added comments to settingsUI

--- Moblima/src/UI/SettingsUI.java
+++ Moblima/src/UI/SettingsUI.java
@@ -38,82 +38,87 @@ public class SettingsUI {
 			System.out.println("8) List top movies.");
 			System.out.println("9) Exit.");
 			choice = sc.nextInt();
-			sc.nextLine(); // eat the \n
+			sc.nextLine(); // Eat the \n
 			switch(choice) {
 			case 1:
-				enums.Day day = DaySettings.setDay();//this variable has to go somewhere but im not sure where
+				enums.Day day = DaySettings.setDay();
 				System.out.println("Day set to " + day); 
 				break;
+			
 			case 2:
-				Movie newMovie = MovieSettings.addMovie(); //create movie
-				mm.addMoviecsv(newMovie); //add to csv
+				Movie newMovie = MovieSettings.addMovie(); //Create movie
+				mm.addMoviecsv(newMovie); //Add to movieInfo CSV
 				break;
+			
 			case 3: 
 				System.out.println("Enter movie title to edit: ");
 				String TitletoEdit = sc.nextLine();
-				movieList = mm.readMovieCSV(); //convert csv to arraylist
-				int j = MovieInfoManager2.findMovieCSV(TitletoEdit, movieList); //find it in array list
-				if(j == -1) //if it does not exist
+				movieList = mm.readMovieCSV(); //Convert CSV to array list
+				int j = MovieInfoManager2.findMovieCSV(TitletoEdit, movieList); //Find it in array list
+				if(j == -1) //If it does not exist
 				{
 					System.out.println("Movie does not exist! Exiting...");
-					break; //exit
+					break; //Exit
 				}
-				Movie MovietoEdit = movieList.get(j); //if it exist, get details
-				movieList.remove(j); //remove it from array list
-				MovieSettings.editMovie(MovietoEdit); //edit movie
-				movieList.add(MovietoEdit); //add it back to array list
-				mm.writeMovieCSV(movieList); //write it to csv
+				Movie MovietoEdit = movieList.get(j); //Otherwise get its details
+				movieList.remove(j); //Remove it from the array list
+				MovieSettings.editMovie(MovietoEdit); //Edit the movie
+				movieList.add(MovietoEdit); //Then add it back to the array list
+				mm.writeMovieCSV(movieList); //Finally, write it to the CSV
 				break;
+			
 			case 4: 
-				String TitletoRemove = MovieSettings.removeMovie();
-				movieList = mm.readMovieCSV(); //convert csv to arraylist
-				int k = MovieInfoManager2.findMovieCSV(TitletoRemove, movieList); //find it in array list
-				if(k == -1) //if it does not exist
+				String TitletoRemove = MovieSettings.removeMovie(); //Find title of Movie to remove
+				movieList = mm.readMovieCSV(); //Convert CSV to array list
+				int k = MovieInfoManager2.findMovieCSV(TitletoRemove, movieList); //Find it in array list	
+				if(k == -1) //If it does not exist
 				{
 					System.out.println("Movie does not exist! Exiting...");
-					break; //exit
+					break; //Exit
 				}
-				movieList.remove(k); //if it exist, remove it from array list
-				mm.writeMovieCSV(movieList); //write it to csv
+				movieList.remove(k); //Otherwise, remove it from array list
+				mm.writeMovieCSV(movieList); //Then write the list it to CSV
 				break;
+				
 			case 5:
-				Showtime newShowtime = ShowtimeSettings.addShowtime(); //get showtime to add
-				sm.addShowtimecsv(newShowtime); //add it to csv
+				Showtime newShowtime = ShowtimeSettings.addShowtime(); //Get show time to add
+				sm.addShowtimecsv(newShowtime); //And add it to CSV
 				break;
+				
 			case 6:
 				System.out.println("Enter showtime to edit.");
-				Showtime toEdit = ShowtimeSettings.createShowtime();
+				Showtime toEdit = ShowtimeSettings.createShowtime(); //Find which show time to edit
 				
-				showList = sm.readShowtimecsv(); //convert csv to array list
-				int i = ShowtimeManager.findShowtimecsv(showList, toEdit);
-				if(i == -1) //if showtime does not exist
+				showList = sm.readShowtimecsv(); //Convert CSV to array list
+				int i = ShowtimeManager.findShowtimecsv(showList, toEdit); //Find show time's position
+				if(i == -1) //If show time does not exist
 				{
 					System.out.println("Showtime does not exist! Exiting...");
-					break; //exit program
+					break; //Exit program
 				}
-				showList.remove(i);//remove the showtime that needs editing in array list
-				sm.writeShowtimecsv(showList); //convert array list to csv
-				ShowtimeSettings.editShowtime(toEdit); //edit the showtime
-				sm.addShowtimecsv(toEdit); //add it to the csv
+				showList.remove(i);//Otherwise remove the show time that needs editing in array list
+				sm.writeShowtimecsv(showList); //Convert array list to CSV
+				ShowtimeSettings.editShowtime(toEdit); //Edit the show time
+				sm.addShowtimecsv(toEdit); //And add the show time to the CSV
 				break;
 			case 7: 
-				Showtime ShowTimetoRemove = ShowtimeSettings.removeShowtime(); //get showtime to remove
-				showList = new ArrayList<Showtime>(); 
-				showList = sm.readShowtimecsv(); //convert csv to array list
-				showList = sm.removeShowtimecsv(showList, ShowTimetoRemove); //remove showtime in array list
-				sm.writeShowtimecsv(showList); //rewrite csv back
+				Showtime ShowTimetoRemove = ShowtimeSettings.removeShowtime(); //Find which show time to remove
+				showList = sm.readShowtimecsv(); //Convert CSV to array list
+				showList = sm.removeShowtimecsv(showList, ShowTimetoRemove); //Remove show time in array list
+				sm.writeShowtimecsv(showList); //Rewrite CSV
 				break;
 			case 8:
 				break;
+			case 9:
+				System.out.println("Exiting settings...");
+				return;
 			default:
+				System.out.println("Invalid input! Exiting settings...");
 				return;
 			
 			}
 		}
 		
-	}
-	public static void main(String [] args) throws FileNotFoundException, IOException {
-		settingsText(); //testing
 	}
 
 }
