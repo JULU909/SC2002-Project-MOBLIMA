9dd5781bb72a979e4cd1bab0161643a68556a88b
Sherylleong <sherylleongkitye@gmail.com>
2022-11-10 23:25:45 +0800

    Revert "Merge branch 'main' of https://github.com/JULU909/SC2002-Project-MOBLIMA"
    This reverts commit 161f3a66b5339b6a9b77d0d3f97d231d2972cbd7, reversing
    changes made to a310c8a4d16bb5ef2f66a1ac6afecf87bed724a1.

--- Moblima/src/Moblima.java
+++ Moblima/src/Moblima.java
@@ -25,7 +25,6 @@ public class Moblima {
             int selection=0;
             switch (choice) {
                 case 1:
-                    while(true){
                     System.out.println(
                             "=====================================================================================\n");
                     System.out.println(
@@ -182,10 +181,6 @@ public class Moblima {
                 bt.printAllTickets(userTickets);
                 bt.individualTicketmenu(userTickets);
                 break;
-            case 2: 
-                String ticketID = bt.getTicketID();
-                Ticket ticket = tk.getTicket(ticketID);
-                bt.printTicket(ticket);
 
 
         }
