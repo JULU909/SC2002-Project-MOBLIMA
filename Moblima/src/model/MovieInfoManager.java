b7f613b5fed9dfe0e70842c0d7c3856ee2adb8fb
THoldon <HTHAM001@e.ntu.edu.sg>
2022-11-11 19:28:45 +0800

    Added update total sales which writes total sales into csv

--- Moblima/src/model/MovieInfoManager.java
+++ Moblima/src/model/MovieInfoManager.java
@@ -337,9 +337,14 @@ public class MovieInfoManager {
 		tempMovie.setAverageRating(avgRating);//Set that rating in movie
 		list.set(i, tempMovie);//Set movie with new values
 		
-		writeMovieCSV(list);//Rewrite into CSV
-    	
-    	
+		writeMovieCSV(list);//Rewrite into CSV	
+    }
+    
+    public void updateTotalSales(String title,int sales) throws FileNotFoundException, IOException {
+    	ArrayList<Movie> list = readMovieCSV();//Convert CSV to Array list of movies
+    	int i = findMovieCSV(title,list);//Find position of that movie in array list
+    	list.get(i).setTotalSales(sales);//Update its sales
+    	writeMovieCSV(list); //Rewrite into CSV
     }
     
     public void printAll(ArrayList<Movie> list) {
