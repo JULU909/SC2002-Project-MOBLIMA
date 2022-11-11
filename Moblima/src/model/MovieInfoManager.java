4ecfbe263c504cc636fa61e7dd30c13beaf6865e
THoldon <HTHAM001@e.ntu.edu.sg>
2022-11-11 19:25:17 +0800

    Closed filewriter

--- Moblima/src/model/MovieInfoManager.java
+++ Moblima/src/model/MovieInfoManager.java
@@ -88,6 +88,10 @@ public class MovieInfoManager {
     	if(review.size()==0)
     	{
     		writer.append("\n");
+        	
+    		//cleanup
+        	writer.flush();
+        	writer.close();
     		return;
     	}
     		
